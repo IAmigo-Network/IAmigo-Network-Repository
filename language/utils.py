f9ce739c15467c4c608f1ea052ff6dd424441c4e
IAmigo-Network <iamigo.assistente@gmail.com>
2026-06-18 21:43:37 -0300

    Preparação para multi-linguas

--- language/utils.py
+++ language/utils.py
@@ -1,23 +1,19 @@
 from pathlib import Path
-from os import getcwd
 import configparser
 import xmltodict
 
 def load_language():
-
-    config_path = Path(__file__) / "data" / "config.ini"
+    config_path = Path(__file__).parent.parent / "data" / "config.ini"
     config = configparser.ConfigParser()
-    config.read(str(config_path))
+    config.read(str(config_path), "utf-8")
+    print(config["a"]["b"])
 
-    xml_path = Path(getcwd()) / "language" / "language_xml" / config['localization']['language']
-    print(xml_path)
+    xml_path = Path(__file__).parent / "language_xml" / config["a"]["b"]
+    print(str(xml_path))
 
     with open(xml_path, 'r', encoding='utf-8') as xml_file:
         xml_content = xml_file.read()
-        
+
     strings = xmltodict.parse(xml_content)
     strings = strings["IAmigo"]
     return strings
-
-strings = load_language()
-print(strings)
