0f7b1e76c1855f97ef3b7316a3787d6c5aafbd33
Allan Puga <142864174+allanpuga@users.noreply.github.com>
2026-07-28 21:09:02 -0300

    atu
    atu

---
+++ tpac/ui/utils.py
@@ -0,0 +1,15 @@
+import os
+
+def limpar_tela():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def exibir_cabecalho(titulo: str):
+    limpar_tela()
+    print("=" * 60)
+    print(f"{titulo.center(60)}")
+    print("=" * 60 + "\n")
+    
+def exibir_barra_status(usuario):
+    print(" ╔══════════════════════════════════════════════════════╗")
+    print(f" ║ 👤 Usuário: {usuario:<15}   🤖 IAmigo Online       ║")
+    print(" ╚══════════════════════════════════════════════════════╝")
